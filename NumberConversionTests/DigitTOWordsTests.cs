596f2518c9ae9ce9f8b840990d47693d586fc891
neevan01 <neevanaveen1998@gmail.com>
2024-06-11 07:55:34 +0545

    Add project files.

---
+++ NumberConversionTests/DigitTOWordsTests.cs
@@ -0,0 +1,46 @@
+﻿using NepaliNumberToWord;
+
+namespace NumberConversionTests
+{
+    public class DigitTOWordsTests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void DigitToWordsEqualTest()
+        {
+            // Assign
+            string expected = "Ninety Nine";
+
+            // Act
+            string actual = CSConversionLogic.DigitToWord(99);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void DigitToWordsMaxTest()
+        {
+            // Assign
+            string expected = "";
+
+            // Act
+            string actual = CSConversionLogic.DigitToWord(99999999999999);
+
+            // Assert
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+
+        [TestCase(10000000000000)]
+        public void DIgitToWord_NotEqualsTest(long number)
+        {
+            string expected = "One Shankha";
+
+            string actual = CSConversionLogic.DigitToWord(number);
+
+            Assert.That(actual, Is.Not.EqualTo(expected));
+        }
+    }
+}
